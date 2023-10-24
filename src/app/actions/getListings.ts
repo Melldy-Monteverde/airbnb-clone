61865fd3f76aec7f5e3fe107b243eafcc80bca6a
Lexther Monteverde <monteverdelex@gmail.com>
2023-10-24 12:00:37 -0300

    add listenings views

--- src/app/actions/getListings.ts
+++ src/app/actions/getListings.ts
@@ -6,9 +6,14 @@ export default async function getListing() {
       orderBy: {
         createdAt: 'desc'
       }
-    })
+    });
 
-    return listings
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+
+    return safeListings;
   } catch (error: any) {
     throw new Error(error)
   }
